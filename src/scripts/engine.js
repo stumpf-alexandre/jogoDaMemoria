5130e0e7230482778f7120ae7ea20f0275a5a971
stumpf-alexandre <als.stumpf@gmail.com>
2024-11-05 23:12:53 -0300

    criando os cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "😎",
+    "😎",
+    "🤐",
+    "🤐",
+    "🤡",
+    "🤡",
+    "👺",
+    "👺",
+    "💀",
+    "💀",
+    "👽",
+    "👽",
+    "👀",
+    "👀",
+    "😵‍💫",
+    "😵‍💫"
+];
+let openCard = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
